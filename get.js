403cf1a9c8dde2a01e593351e79d9ebc2800d69b
yyk <yyk@mail.ru>
2021-08-30 18:59:25 -0400

    get module linted

--- get.js
+++ get.js
@@ -1,19 +1,19 @@
-const parse = require("node-html-parser").parse;
+const parse = require('node-html-parser').parse
 
-let distribution_map = {};
+const distributionMap = {}
 const dmap = async function (html) {
-  const root = parse(html);
-  const rows = root
-    .querySelectorAll("tr")
+  const root = parse(html)
+  root
+    .querySelectorAll('tr')
     .slice(2)
-    .map((row) => {
-      let cols = row.querySelectorAll("td");
-      let country = cols[0].querySelector("div").text.trim();
-      let part = cols[3].querySelector("span").text.trim();
-      let percentage = Number(part.slice(0, -1));
-      distribution_map[country] = percentage;
-    });
-  return distribution_map;
-};
+    .forEach((row) => {
+      const cols = row.querySelectorAll('td')
+      const country = cols[0].querySelector('div').text.trim()
+      const part = cols[3].querySelector('span').text.trim()
+      const percentage = Number(part.slice(0, -1))
+      distributionMap[country] = percentage
+    })
+  return distributionMap
+}
 
-exports.dmap = dmap;
+exports.dmap = dmap
