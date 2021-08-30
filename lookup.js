01e56e281b065f47aec15ae8be91954d087aeee9
yyk <yyk@mail.ru>
2021-08-30 15:28:17 -0400

    first commit

---
+++ lookup.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process')
+const axios = require('axios')
+
+const getIp = (domain) => {
+  return new Promise((resolve, reject) => {
+    exec('dig +short ' + domain, (error, stdout, stderr) => {
+      if (error) { reject(error) }
+      if (stderr) { reject(stderr) }
+      const lines = stdout.split('\n')
+      resolve(lines[lines.length - 2])
+    })
+  })
+}
+
+exports.country = (domain) => {
+  return new Promise((resolve, reject) => {
+    getIp(domain)
+      .then((ip) => {
+        axios.get('http://www.iplocate.io/api/lookup/' + ip)
+          .then((x) => {
+            resolve(x.data.country)
+          })
+          .catch(e => reject(e))
+      })
+  })
+}
