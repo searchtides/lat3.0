ecc940af267f31a7d9777f9558d8e6c976c146a6
yyk <yyk@mail.ru>
2021-11-15 12:01:01 -0400

    fix: blacklisted set corrected

--- modules/blackListFilter.js
+++ modules/blackListFilter.js
@@ -2,8 +2,10 @@ const _ = require('underscore')
 const main = (domains, blackList) => {
   const blackMap = _.object(blackList, blackList)
   const whiteList = domains.filter(domain => blackMap[domain] === undefined)
+  const toRemove = domains.filter(domain => blackMap[domain] !== undefined)
   const succeed = _.object(whiteList, whiteList)
-  return { right: { succeed, blacklisted: blackMap } }
+  const blacklisted = _.object(toRemove, toRemove)
+  return { right: { succeed, blacklisted } }
 }
 
 module.exports = main
