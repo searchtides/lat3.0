08c49a6502d010953c0f093da5076066fc742fdb
yyk <yyk@mail.ru>
2021-11-23 11:11:03 -0400

    fix: skipping spam and keyword phase if no spam or keywrods as well as domains

--- modules/spamDetector.js
+++ modules/spamDetector.js
@@ -18,22 +18,27 @@ async function main ({ right, left }, spamKeywords, pathToRegister, logger) {
   if (left) { // forwarding error
     return Promise.resolve({ right, left })
   }
-  const { succeed, rejected, failed } = right
-  const domains = _.keys(succeed)
-  const blacklisted = right.blacklisted
+
   logger({ type: 'phase', data: { name: PHASE, type: 'singleAttempt' } })
-  logger({ type: 'blockSize', data: domains.length })
-  const funcs = domains.map(domain => () => detectSpam(domain, spamKeywords, pathToRegister))
-  const res = await serial(funcs, logger)
-  const success = res.map(x => x.right).filter(x => x)
-  const succeedMap = makeMap(success)
-  _.keys(succeedMap).forEach(domain => {
-    succeedMap[domain] = _.extend({}, succeedMap[domain], succeed[domain])
-  })
-  const fail = res.filter(x => x.left).map(x => _.extend({}, x.left, { phase: PHASE }))
-  const failMap = makeMap(fail)
-  const failedNext = _.extend({}, failed, failMap)
-  return { right: { succeed: succeedMap, rejected, failed: failedNext, blacklisted } }
+  const { succeed, rejected, failed, blacklisted } = right
+  const domains = _.keys(succeed)
+  if (spamKeywords.length !== 0 && domains.length !== 0) {
+    logger({ type: 'blockSize', data: domains.length })
+    const funcs = domains.map(domain => () => detectSpam(domain, spamKeywords, pathToRegister))
+    const res = await serial(funcs, logger)
+    const success = res.map(x => x.right).filter(x => x)
+    const succeedMap = makeMap(success)
+    _.keys(succeedMap).forEach(domain => {
+      succeedMap[domain] = _.extend({}, succeedMap[domain], succeed[domain])
+    })
+    const fail = res.filter(x => x.left).map(x => _.extend({}, x.left, { phase: PHASE }))
+    const failMap = makeMap(fail)
+    const failedNext = _.extend({}, failed, failMap)
+    return { right: { succeed: succeedMap, rejected, failed: failedNext, blacklisted } }
+  } else {
+    logger({ type: 'message', data: 'skip' })
+    return { right: { succeed, rejected, failed, blacklisted } }
+  }
 }
 
 module.exports = main
